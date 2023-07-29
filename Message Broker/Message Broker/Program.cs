e36f9495604857305ac47e61213cd9e746ae7da8
harsh_git <guptaharsh686@gmail.com>
2023-07-29 13:52:50 +0530

    Done with apis to get subscription and messages for subscriptions

--- Message Broker/Message Broker/Program.cs
+++ Message Broker/Message Broker/Program.cs
@@ -63,4 +63,83 @@ app.MapPost("api/topics/{id}/messages", async (AppDbContext context, int id, Mes
 });
 
 
+// Create Subscription
+
+app.MapPost("api/topics/{id}/subscriptions", async (AppDbContext context, int id, Subscription sub) =>
+{
+    bool topic = await context.Topics.AnyAsync(t => t.Id == id);
+    if (!topic)
+    {
+        return Results.NotFound("Topic Not Found");
+    }
+
+    sub.TopicId = id;
+
+    await context.Subscriptions.AddAsync(sub);
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/topics/{id}/subscriptions/{sub.Id}", sub);
+});
+
+//Get Subscriber Messages
+
+app.MapGet("api/subscriptions/{id}/messages", async (AppDbContext context, int id) =>
+{
+    bool subs = await context.Subscriptions.AnyAsync(s => s.Id == id);
+    if (!subs)
+    {
+        return Results.NotFound("Subscription not found");
+    }
+
+    var messages = context.Messages.Where(m => m.SubscriptionId == id && m.MessageStatus != "SENT");
+
+    if(messages.Count() == 0)
+    {
+        return Results.NotFound("No New Messages");
+    }
+
+    foreach (var msg in messages)
+    {
+        msg.MessageStatus = "REQUESTED";
+
+    }
+
+    await context.SaveChangesAsync();
+
+    return Results.Ok(messages);
+});
+
+
+// Acknowledge messages for subscriber
+
+app.MapPost("api/subscriptions/{id}/messages", async (AppDbContext context, int id, int[] confs) =>
+{
+    bool subs = await context.Subscriptions.AnyAsync(s => s.Id == id);
+    if (!subs)
+    {
+        return Results.NotFound("Subscription not found");
+    }
+
+    if(confs.Length <= 0)
+    {
+        return Results.BadRequest();
+    }
+
+    int count = 0;
+
+    foreach (var msgid in confs) 
+    {
+        var msg = context.Messages.FirstOrDefault( m => m.Id == msgid);
+        if(msg != null)
+        {
+            msg.MessageStatus = "SENT";
+            await context.SaveChangesAsync();
+            count++;
+        }
+    }
+
+    return Results.Ok($"Acknowledged {count}/{confs.Length} messages");
+
+});
+
 app.Run();
